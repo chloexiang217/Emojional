35b3a22c52b354d135505e239e4ba9c6c9c0ef94
Chloe Xiang <chloe.xiang217@gmail.com>
2019-06-23 14:38:22 +0200

    Complete Iteration 3

--- Emojional/ViewController.swift
+++ Emojional/ViewController.swift
@@ -11,11 +11,17 @@ import UIKit
 class ViewController: UIViewController {
 
     let emojis = ["🥰": "super love", "🦋": "butterfly"]
+    let customMessages = ["super love" : ["love you", "ily", "so much love"], "butterfly" : ["fly high", "wings were meant to fly", "soaring high"]]
+    
     
     @IBAction func showMessage(sender: UIButton) {
     let selectedEmotion = sender.titleLabel?.text
     let message = emojis[selectedEmotion!]!
-    let alertController = UIAlertController(title: "Hello", message: message, preferredStyle: UIAlertController.Style.alert)
+    let random = customMessages[emojis[selectedEmotion!]!]!.count
+    let emojiMessage = customMessages[emojis[selectedEmotion!]!]?[Int.random(in: 0 ... random-1)]
+    
+        
+    let alertController = UIAlertController(title: "\(message)", message: emojiMessage, preferredStyle: UIAlertController.Style.alert)
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
     present(alertController, animated: true, completion: nil)
         
