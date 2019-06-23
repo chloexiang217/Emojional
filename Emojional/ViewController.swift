adbf2ba35445f633ff8e86ff1d94650cbe709879
Chloe Xiang <chloe.xiang217@gmail.com>
2019-06-23 13:44:45 +0200

    Initial Commit

---
+++ Emojional/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Emojional
+//
+//  Created by Chloe Xiang on 6/23/19.
+//  Copyright © 2019 Chloe Xiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
